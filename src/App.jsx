ba0058ab75fa80fd9c56bd1729014c40f67db7df
Pedro36674 <pedrohenrique25055@gmail.com>
2026-05-14 18:43:56 -0300

    Interface inicial

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import Header from './components/Header'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App dark:bg-gray-800 text-white min-h-screen">
+      <Header />
+      <h1 className="text-4xl font-bold p-4">OFERTAS</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+
+function Header() {
+    return (
+        <header className="header bg-mist-900 px-12 text-2xl text-white gap-4 p-4">
+            <h3 className="hover:text-gray-500">LOJA</h3>
+            <h3 className="hover:text-gray-500">BIBLIOTECA</h3>
+            <h3 className="hover:text-gray-500">COMUNIDADE</h3>
+            <h3 className="hover:text-gray-500">AJUDA</h3>
+            <script></script>
+        </header>
+    );
+}
+
+export default Header;
