39681eb48d59e55b6d9f1c82d057724a000c17da
Pedro Henrique <pedrohenrique25055@gmail.com>
2026-06-11 16:50:43 -0300

    Adicionando jogos com dados fakes

---
+++ src/data/Jogos.js
@@ -0,0 +1,72 @@
+export const Jogos = [
+    {
+        id: 1,
+        name: 'Forza Horizon 5',
+        description: 'Explore as paisagens vibrantes de mundo aberto do México com diversão e velocidade sem limites com os melhores carros do mundo.',
+        price: 149.90,
+        coverImage: 'https://cdn.forza.net/strapi-uploads/assets/FH_5_Evergreen_Key_Art_Horizontal_3840x2160_RGB_337636ab7f.jpg',
+        bannerImage: 'https://playtracker.net/cdn-cgi/image/format=auto/https://playtracker-apollo.fra1.cdn.digitaloceanspaces.com/covers/api/game_cover_70470.jpg',
+        category: 'Corrida',
+        featured: false
+    },
+    {
+        id: 2,
+        name: 'The Witcher 3: Wild Hunt',
+        description: 'Embarque em uma jornada épica como Geralt de Rívia, um caçador de monstros, em um mundo aberto repleto de perigos e intrigas.',
+        price: 99.90,
+        coverImage: 'https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/292030/ad9240e088f953a84aee814034c50a6a92bf4516/header.jpg?t=1768303991',
+        bannerImage: 'https://resources.crowdcontrol.live/images/Witcher3/banner.jpg',
+        category: 'RPG',
+        featured: true
+    },
+    {
+        id: 3,
+        name: 'Cyberpunk 2077',
+        description: 'Mergulhe em Night City, uma metrópole futurista e perigosa, onde você pode ser qualquer um, fazer qualquer coisa e ser qualquer um.',
+        price: 129.90,
+        coverImage: 'https://cdn.akamai.steamstatic.com/steam/apps/1091500/capsule_616x353.jpg',
+        bannerImage: 'https://cdn.akamai.steamstatic.com/steam/apps/1091500/capsule_616x353.jpg',
+        category: 'RPG',
+        featured: false
+    },
+    {
+        id: 4,
+        name: 'Elden Ring',
+        description: 'Explore o reino de Elden Ring, um mundo aberto cheio de aventuras e desafios, onde você pode ser qualquer um e fazer qualquer coisa.',
+        price: 149.90,
+        coverImage: 'https://cdn.akamai.steamstatic.com/steam/apps/1245620/capsule_616x353.jpg',
+        bannerImage: 'https://cdn.akamai.steamstatic.com/steam/apps/1245620/capsule_616x353.jpg',
+        category: 'RPG',
+        featured: false
+    },
+    {
+        id: 5,
+        name: 'LEGO Batman: O Legado do Cavaleiro das Trevas',
+        description: 'Assuma o papel do Cavaleiro das Trevas e viva a história essencial do Batman em uma aventura ousada e cheia de ação, com combate intenso, uma Gotham City de mundo aberto e o charme característico da LEGO que os fãs conhecem e amam.',
+        price: 199.90,
+        coverImage: 'https://image.api.playstation.com/vulcan/ap/rnd/202511/1118/f4e6951f2326e94a5f5d4bd4d54979f5421d6e34327849f2.jpg',
+        bannerImage: 'https://www.lego.com/cdn/cs/set/assets/blt18519b2b57cf2876/02_LEGO_Dinner-dark_Hero-Standard_Desktop_v02.jpg',
+        category: 'Ação',
+        featured: false
+    },
+    {
+        id: 6,
+        name: 'Assassin\'s Creed Valhalla',
+        description: 'Explore o mundo viking com Ezio Auditore, um assassino que enfrenta a corrupção e a opressão em um mundo em transformação.',
+        price: 149.90,
+        coverImage: 'https://cdn1.epicgames.com/400347196e674de89c23cc2a7f2121db/offer/AC%20KINGDOM%20PREORDER_STANDARD%20EDITION_EPIC_Key_Art_Wide_3840x2160-3840x2160-485fe17203671386c71bde8110886c7d.jpg',
+        bannerImage: 'https://staticctf.ubisoft.com/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/5KsyqEOChSmgAGGqcz7opE/3920505781b0a6aed2f3b74216ef1f72/ACV_KeyArt11__960x540.png',
+        category: 'Ação',
+        featured: false
+    },
+    {
+        id: 7,
+        name: 'Red Dead Redemption 2',
+        description: 'Viva a vida de um fora da lei no Velho Oeste, enfrentando desafios, fazendo escolhas e vivendo uma história épica de redenção.',
+        price: 149.90,
+        coverImage: 'https://cdn.akamai.steamstatic.com/steam/apps/1174180/capsule_616x353.jpg',
+        bannerImage: 'https://cdn2.steamgriddb.com/hero/964e7520947a0d3ac39504daea604d83.png',
+        category: 'Ação',
+        featured: false
+    }
+]
