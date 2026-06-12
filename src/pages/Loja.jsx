d4b4cb4ba82e8f4be28da6ecbe31f78fc8298304
Pedro Henrique <pedrohenrique25055@gmail.com>
2026-06-12 15:32:25 -0300

    Mesclando componentes na pagina de loja

---
+++ src/pages/Loja.jsx
@@ -0,0 +1,29 @@
+import Header from '../components/Header'
+import BannerOfertas from '../components/BannerOfertas'
+import JogoCard from '../components/JogoCard'
+import { Jogos } from '../data/Jogos'
+import InfoJogo from './InfoJogo'
+
+function Loja() {
+     const featuredGame = Jogos.find((game) => game.featured) || Jogos[0]
+    const otherGames = Jogos.filter((game) => !game.featured)
+
+
+    return (
+        <div className="App min-vh-100 bg-steam-body-dark text-white">
+      <Header />
+
+      <main className="container p-0">
+        <h1 className="display-5 fw-bold fs-1 mt-4">JOGOS EM DESTAQUE</h1>
+        <BannerOfertas game={featuredGame} />
+
+        <section className="mt-5">
+          <h2 className="fs-3 mb-3">Outros jogos</h2>
+          <JogoCard games={otherGames} />
+        </section>
+      </main>
+    </div>
+    )
+}
+
+export default Loja;
